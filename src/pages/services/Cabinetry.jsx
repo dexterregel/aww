a266ee2ca78b001972c155cff584af5287cd57e5
Dexter Regel <dexter.regel@gmail.com>
2026-07-05 20:21:12 -0400

    testing importing images

--- src/pages/services/Cabinetry.jsx
+++ src/pages/services/Cabinetry.jsx
@@ -1,21 +1,23 @@
 
 
 export default function Cabinetry() {
-  const cabinetryImages = import.meta.glob('/src/assets/images/cabinetry/*');
-  const images = Object.keys(cabinetryImages).map((path, index) => {
-    return (
-      <img key={index} src={path} />
-    );
+  const modules = import.meta.glob(
+    '../../assets/images/cabinetry/*',
+    { eager: true }
+  );
+  console.log(Object.values(modules));
 
-  })
+  const images = Object.values(modules).map(module => module.default);
 
+  const imageEls = images.map((image, index) => <img key={index} src={image} />);
+  
   return (
     <main>
       <section>
         <h1>Cabinetry</h1>
         <p>Architectural Woodworking's Cabinetry Division is the perfect answer for everything from face frame and european style cabinetry to custom finishes and solid surfaces. We back all cabinetry work with a full selection of styles, materials, accessories, and finishes. Our Cabinetry Division allows you to take the guess work out of ordering by delivering exactly what you need. Remember when you are working with Architectural Woodworking, we have the expertise and on site facilities to provide you with the product you require!</p>
         <div>
-          {images}
+          {imageEls}
         </div>
       </section>
     </main>
