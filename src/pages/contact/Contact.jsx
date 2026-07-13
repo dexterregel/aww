5a4e24929d85b7bdc8f6a6dc92554ff45b1f00d9
Dexter Regel <dexter.regel@gmail.com>
2026-07-13 13:52:57 -0400

    implemented static google map

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,15 +1,18 @@
 import './contact.css';
 
 export default function Contact() {
-  const searchString = 'Architectural+Woodworking';
+  // google maps vars
+  const googleMapsBaseUrl = 'http://maps.googleapis.com/maps/api/staticmap';
+  const googleMapsMarker = 'markers=color:red%7CArchitectural+Woodworking'; // %7C encodes to a pipe char
+  const googleMapsZoom = 'zoom=15';
   const googleMapsApiKey = 'AIzaSyA-61VXRWb0qiWrZAcEd3385K7rJEfoSVE';
-  const googleMapsUrl = `http://maps.googleapis.com/maps/api/staticmap?center=${searchString}&zoom=15&size=600x300&maptype=roadmap&key=${googleMapsApiKey}`;
+  const googleMapsUrl = `${googleMapsBaseUrl}?${googleMapsMarker}&${googleMapsZoom}&style=feature:poi|visibility:off&size=600x300&key=${googleMapsApiKey}`;
 
   return (
     <main>
       <h1>Contact us</h1>
+      <img src={googleMapsUrl} />
       <ul className='contact-container'>
-        <img src={googleMapsUrl} />
         <li>Architectural Woodworking</li>
         <li>3291 40th Ave N</li>
         <li>St. Petersburg, FL 33714</li>
